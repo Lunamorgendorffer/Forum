df775dbf38dcec5c12a04f19ab4e67f9cf6679ab
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-25 18:30:53 +0200

    ajout chemin des vues lists

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -8,6 +8,7 @@
     use Model\Managers\TopicManager;
     use Model\Managers\PostManager;
     use Model\Managers\UserManager;
+    use Model\Managers\CatManager;
     
     class ForumController extends AbstractController implements ControllerInterface{
 
@@ -15,15 +16,35 @@
           
 
            $topicManager = new TopicManager();
-           $userManager = new userManager();
-
+           $userManager = new UserManager();
+           $postManager = new PostManager();
+           $catManager = new CatManager();
+        
             return [
                 "view" => VIEW_DIR."forum/listTopics.php", // renvoie la vue listtopics
                 "data" => [ //data prend la valeur d'un tableau qui contient topics 
-                    "topics" => $topicManager->findAll(["creationdate", "DESC"]), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
-                    "users" => $userManager->findAll(["registerDate", "DESC"]) //Dans useManager va me chercher la fonc findAll, trié par creation date 
-                ]
+                    // "topics" => $topicManager->findAll(["creationdate", "DESC"]), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
+                    "topics" => $topicManager->findAllTopicsUser(), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
+                ],
+
+                "view" => VIEW_DIR."forum/listUsers.php", // envoi à la vue listUsers
+                "data" => [ //data prend la valeur d'un tableau qui contient Users
+                    "users" => $userManager->findAll(), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
+                ],
+
+                "view" => VIEW_DIR."forum/listPosts.php", // envoi à la vue listUsers
+                "data" => [ //data prend la valeur d'un tableau qui contient Users
+                    "post" => $postManager->findAllPostUser(), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
+                ],
+
+                "view" => VIEW_DIR."forum/listCategory.php", // envoi à la vue listUsers
+                "data" => [ //data prend la valeur d'un tableau qui contient Users
+                    "category" => $catManager->findAll(), // Dans topicManager va me chercher la fonc findAll, trié par creation date, 
+                ],
             ];
+
+           
+            
         
         }
 
