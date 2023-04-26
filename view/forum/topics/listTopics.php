e999799008c90e5295c5ed51e920b054fd934ca9
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-26 16:44:08 +0200

    changement dossier

--- view/forum/topics/listTopics.php
+++ view/forum/topics/listTopics.php
@@ -1,6 +1,7 @@
 <?php
 
 $topics = $result["data"]['topics'];
+
 // var_dump($topics)
 
     
@@ -8,16 +9,21 @@ $topics = $result["data"]['topics'];
 
 <h1>liste topics</h1>
 <div class="container">
-<?php foreach($topics as $topic ) {?>
+<?php foreach($topics as $topic ) {
+  $idTopic = $topic->getId();
+  // var_dump($topic->getId()) 
+  ?>
+ 
 <div class="card  " >
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title "><a href="#"><?=$topic->getTitle()?></a></h5>
+    <h5 class="card-title "><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitle()?></a></h5>
+
     <p class="card-text"><?=$topic->getCreationdate()?></p>
     <p class="card-text"><?=$topic->getUser()->getPseudo()?></p>
     <a href="#" class="btn btn-primary">Add</a>
   </div>
-  <?php } ?>
+  <?php var_dump($idTopic); } ?>
 </div>
 </div>
 
