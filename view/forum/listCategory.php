6f22aeaec67db4d0ee1f7423e4e060fc6d0c9312
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-26 17:50:16 +0200

    changement de style

--- view/forum/listCategory.php
+++ view/forum/listCategory.php
@@ -6,15 +6,19 @@ $categories = $result["data"]['categories'];
 
 <h1>liste Categories</h1>
 
-<?php
-foreach($categories as $category ){
-    // var_dump($posts)
 
-    ?>
-    <p><?=$category -> getNameCategory()?></p>
-   <?php
-}
 
+<h1 style="text-align: center; color: white; margin-bottom: 30px">Listes des catégories</h1>
+<div class="container" style="display: flex;flex-wrap: wrap;">
+    <?php foreach($categories as $category){ ?>
+    <div class="card" style="background-image: url('https://i.pinimg.com/originals/c9/6d/09/c96d09dd9e2ac87f10301cb40f94e8d3.jpg'); width: 30%; margin: 25px; ">
+    <img img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title" style ="text-align: center; color: white;">catégorie</h5>
+        <h3 class="card-text" style="text-align: center; "><a style="color: white;" href="#"><?=$category -> getNameCategory()?></a><br></h3>
+    </div>
+</div>
+<?php } ?>
 
 
   
--- view/forum/topics/listTopics.php
+++ view/forum/topics/listTopics.php
@@ -7,25 +7,20 @@ $topics = $result["data"]['topics'];
     
 ?>
 
-<h1>liste topics</h1>
-<div class="container">
-<?php foreach($topics as $topic ) {
-  $idTopic = $topic->getId();
-  // var_dump($topic->getId()) 
-  ?>
- 
-<div class="card  " >
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title "><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitle()?></a></h5>
 
-    <p class="card-text"><?=$topic->getCreationdate()?></p>
-    <p class="card-text"><?=$topic->getUser()->getPseudo()?></p>
-    <a href="#" class="btn btn-primary">Add</a>
-  </div>
-  <?php var_dump($idTopic); } ?>
+<h1 style="text-align: center; color: white; margin-bottom: 30px">liste topics</h1>
+<div class="container" style="display: flex;flex-wrap: wrap;">
+    <?php foreach($topics as $topic){ ?>
+    <div class="card" style="background-image: url('https://i.pinimg.com/originals/c9/6d/09/c96d09dd9e2ac87f10301cb40f94e8d3.jpg'); width: 30%; margin: 25px; ">
+    <img img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title" style ="text-align: center;"><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitle()?></a></h5>
+        <p class="card-text" style ="text-align: center;"><?=$topic->getCreationdate()?></p>
+        <p class="card-text" style ="text-align: center;"><?=$topic->getUser()->getPseudo()?><br></p>
+    </div>
 </div>
-</div>
+<?php } ?>
+
 
 
 
