457f6f6999a5d7383b98e0706f31934224a2df48
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-26 10:06:56 +0200

    clean

--- view/forum/listCategory.php
+++ view/forum/listCategory.php
@@ -1,10 +1,10 @@
 <?php
 
-$categories = $result["data"]['category'];
+$categories = $result["data"]['categories'];
     
 ?>
 
-<h1>liste message</h1>
+<h1>liste Categories</h1>
 
 <?php
 foreach($categories as $category ){
