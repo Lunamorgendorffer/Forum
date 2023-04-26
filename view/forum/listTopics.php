9d62573aebc166be7e9821cc2d421ca17e35e030
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-26 11:26:27 +0200

    style bootstrap

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -7,15 +7,32 @@ var_dump($topics)
 ?>
 
 <h1>liste topics</h1>
+<div class="container">
+    <div class="row">
+        <table class="table table-striped mt-4 display">
+            <thead>
+                <tr class="bg-secondary">
+                    <th scope="col">Topics</th>
+                    <th scope="col">Publié le</th>
+                    <th scope="col">Ecrit par</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($topics as $topic ) {?>
+                <td scope="row"><a href="#"><?=$topic->getTitle()?></td>
+                <td scope="row"><?=$topic->getCreationdate()?></a></td>
+                <td scope="row"><?=$topic->getUser()->getPseudo()?></a></td>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
-<?php
-foreach($topics as $topic ){
-    // var_dump($topics)
 
-    ?>
-    <p><?=$topic->getTitle()." ".$topic->getCreationdate()." de ".$topic->getUser()->getPseudo()."<br>"?></p>
-   <?php
-}
+
 
 
 
