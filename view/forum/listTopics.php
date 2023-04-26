1c705b273b2661bb724d89bf4e50e919f4d4444f
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-26 14:16:49 +0200

    Changement style

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,35 +1,26 @@
 <?php
 
 $topics = $result["data"]['topics'];
-var_dump($topics)
+// var_dump($topics)
 
     
 ?>
 
 <h1>liste topics</h1>
 <div class="container">
-    <div class="row">
-        <table class="table table-striped mt-4 display">
-            <thead>
-                <tr class="bg-secondary">
-                    <th scope="col">Topics</th>
-                    <th scope="col">Publié le</th>
-                    <th scope="col">Ecrit par</th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach($topics as $topic ) {?>
-                <td scope="row"><a href="#"><?=$topic->getTitle()?></td>
-                <td scope="row"><?=$topic->getCreationdate()?></a></td>
-                <td scope="row"><?=$topic->getUser()->getPseudo()?></a></td>
-            <?php } ?>
-            </tbody>
-        </table>
-    </div>
+<?php foreach($topics as $topic ) {?>
+<div class="card  " >
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title "><a href="#"><?=$topic->getTitle()?></a></h5>
+    <p class="card-text"><?=$topic->getCreationdate()?></p>
+    <p class="card-text"><?=$topic->getUser()->getPseudo()?></p>
+    <a href="#" class="btn btn-primary">Add</a>
+  </div>
+  <?php } ?>
 </div>
+</div>
+
 
 
 
