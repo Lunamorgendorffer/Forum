12124aa1471f4177f50ddd312e023e13e5ffae6f
Lunamorgendorffer <nabdou68@gmail.com>
2023-04-28 16:39:31 +0200

    first commit

---
+++ view/forum/topics/ajouterPostByTopic.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+<h3>Add Post</h3>
+<form action="index.php?action=AddPostByTopic" method="post">
+
+    <p>
+        <label>New Post</label>
+        <textarea id="post" name="post" rows="4" cols="50"></textarea>
+    </p>
+    
+
+    <p>
+        <input type="submit" name="submit" value="Ajouter">
+    </p>
+    
+</form>
+
+
