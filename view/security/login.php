fdeb859c2cb1e5afb4da17260844d1d6ee323456
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-02 12:32:27 +0200

    clean

--- view/security/login.php
+++ view/security/login.php
@@ -1,4 +1,5 @@
 <h1>form connexion</h1>
+
 <form action="index.php?ctrl=security&action=register" method="post" enctype ="multipart">
     
     <input type="text" name="pseudo" placeholder="pseudo" required>
