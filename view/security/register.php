e1503b44d1c4d3461ac0205d90a086e4a64681cc
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-02 13:50:36 +0200

    rename

---
+++ view/security/register.php
@@ -0,0 +1,15 @@
+<h1>form connexion</h1>
+
+<form action="index.php?ctrl=security&action=register" method="post" enctype ="multipart">
+    
+    <input type="text" name="pseudo" placeholder="pseudo" required>
+    
+    <input type="email" name="email" placeholder="email" required>
+    
+    <input type="password" name="password" placeholder="password" required>
+    
+    <input type="password" name="confirmPassword" placeholder="confirmPassword" required>
+    
+    <button type="submit">Valider</button>
+
+</form>
