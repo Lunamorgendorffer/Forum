2f2deb6cf10b8dd7dc0ba54fab86e28c80897c19
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-03 22:14:59 +0200

    update form

--- view/security/login.php
+++ view/security/login.php
@@ -1,9 +1,10 @@
 <h1>Connectez-vous</h1>
-<form action="index.php?ctrl=security&action=login">
+<form action="index.php?ctrl=security&action=login" method="post">
 
-    <input type="text" name="pseudo" placeholder="email" required>
+    <input type="text" name="pseudo" placeholder="pseudo" required>
     
     <input type="password" name="password" placeholder="password" required>
 
     <button type="submit">Valider</button>
 </form>
+
