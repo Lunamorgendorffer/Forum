f510372c22ae1767d5436727091760f5e4ba7f84
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-03 22:13:27 +0200

    lien viewAdd

--- view/forum/listTopicsByCategory.php
+++ view/forum/listTopicsByCategory.php
@@ -18,11 +18,13 @@ echo "<h1 style='text-align: center; color: white; margin-bottom: 30px'>" .$cate
     <div class="card" style="background-image: url('https://i.pinimg.com/originals/c9/6d/09/c96d09dd9e2ac87f10301cb40f94e8d3.jpg'); width: 30%; margin: 25px; ">
     <img img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-        <h5 class="card-title" style ="text-align: center;"><a href="index.php?ctrl=forum&action=detailTopic&id=<?= $topic->getId()?>"><?=$topic->getTitle()?></a></h5>
+        <h5 class="card-title" style ="text-align: center;"><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$topic->getId()?>"><?=$topic->getTitle()?></a></h5>
         <p class="card-text" style ="text-align: center;"><?=$topic->getCreationdate()?></p>
         <p class="card-text" style ="text-align: center;"><?=$topic->getUser()->getPseudo()?><br></p>
     </div>
 </div>
 <?php } ?>
-
+<div class="container" style="display: flex;justify-content: center;;">
+    <a  class="btn btn-primary" href="index.php?ctrl=forum&action=viewAddTopic&id=<?=$_GET['id']?>">Add</a>
+</div>
 
