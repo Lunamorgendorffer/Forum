251bf44c9b3764865aa226131a672f3e6111e858
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-04 20:54:49 +0200

    lien edit +delete

--- view/forum/topics/detailTopic.php
+++ view/forum/topics/detailTopic.php
@@ -2,28 +2,35 @@
 
 $topic = $result["data"]['topic'];
 $messages = $result["data"]["messages"];
+// var_dump($messages);
 var_dump($messages);
 
-// var_dump($messages);
+
  
 ?>
 
 
 <h1 style="text-align: center; margin-bottom: 30px">titre</h1>
 <div class="container" style="display: flex;flex-direction: column;align-items: center;/* justify-content: center; */">
-    <?php foreach ($messages as $message){ ?>
+    <?php foreach ($messages as $message){ 
+        // var_dump($message);?>
     <div class="card" style="background-image: url('https://i.pinimg.com/originals/c9/6d/09/c96d09dd9e2ac87f10301cb40f94e8d3.jpg'); width: 30%; margin: 25px; ">
     <img img src="..." class="card-img-top" alt="...">
     <div class="card-body">
         <p class="card-text" style="text-align: center; color: white; "><strong><?=$message->getMessage()?></strong></p>
         <p class= "card-text" style ="text-align: center; color: white;"></small><?=$message->getUser()->getPseudo()?></small></p>
     </div>
-    <?php if($topic->getUser()->getId() == App\Session::getUser()){?>
+    <?php //if($message->getUser()->getPseudo() == App\Session::getUser()){
+        // echo " <div class='button' style='display: flex;justify-content: center;'>";
+        // echo"<button type='button' class='btn btn-secondary btn-sm'>Edit</button>";
+        // echo "<a href='index.php?ctrl=forum&action=deletePost&id='.$message->getId().'>Delete</a> </div>";
+    //}?>
+    
     <div class="button" style="display: flex;justify-content: center;">
-        <button type="button" class="btn btn-secondary btn-sm">Edit</button>
-        <a href="index.php?ctrl=forum&action=deletePost&id=<?=$message->getId()?>">Delete</a>
+        <a  class="btn btn-secondary" href="index.php?ctrl=forum&action=viewEditPost&id=<?=$_GET['id']?>">Edit</a>
+        <a  class="btn btn-danger" href="index.php?ctrl=forum&action=deletePost&id=<?=$message->getId()?>"> Delete</a>
     </div>
-    <?php } ?>
+    
 </div>
 <?php } ?>
 
